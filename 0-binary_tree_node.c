560a0cf1a04ee51ff059d119be3f7832a1e9bb89
oniaz <32391049+oniaz@users.noreply.github.com>
2024-02-01 04:33:28 +0200

    task 0 - creating a new node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node -  creates a binary tree node.
+ * @parent: pointer to the parent node of the node to be created.
+ * @value: the value of the new node.
+ *
+ * Return: pointer ot the new node, or NULL on failure.
+ **/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = NULL;
+
+	node = malloc(sizeof(binary_tree_t));
+	if (!node)
+		return (NULL);
+
+	node->parent = parent;
+	node->n = value;
+	return (node);
+}
