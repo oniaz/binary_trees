3ec14ba3eb846e0ea98cb6156e17bd65470976ff
oniaz <32391049+oniaz@users.noreply.github.com>
2024-02-01 04:34:42 +0200

    setting children nodes to null?

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -16,6 +16,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		return (NULL);
 
 	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
 	node->n = value;
 	return (node);
 }
